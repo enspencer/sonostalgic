96c24e69cabd21bbdee3b4e0ba54de20c254e35c
Emma Spencer <emma.n.spencer@gmail.com>
2013-12-12 13:35:35 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sonostalgic::Application.config.secret_key_base = '96f2ab6794c8fca2184879d42c0161b664b08dc0dee01498d9a7e02bb3ac30ed7fc60922d71338c2a4100e02d68940ab20bd4e28081d154f3bed83145e5a8b6e'
