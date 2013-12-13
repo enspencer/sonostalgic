ba2b903d8c6dbd9cf332273eb56a39ffc8936a58
Emma Spencer <emma.n.spencer@gmail.com>
2013-12-13 12:23:47 -0500

    working on add event to timeline

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,11 +1,17 @@
 class TimelinesController < ApplicationController
-  before_action :set_timeline, only: [:show]
+  before_action :set_timeline, only: [:show, :update]
 
   def index
   end
 
   def show
     @timeline = Timeline.find(params[:id])
+    @events = @timeline.events
+    @artists = []
+    @timeline.events.each do |event|
+      @artists << event.artists
+    end
+    
   end
 
   def new
@@ -14,6 +20,9 @@ class TimelinesController < ApplicationController
 
   def create
     @timeline = Timeline.new(timeline_params)
+    if(!params.has_key?(:title))
+      @timeline.title = @timeline.id
+    end
     respond_to do |format|
     if @timeline.save
       format.html { redirect_to @timeline, notice: 'timeline was successfully created.' }
@@ -25,6 +34,20 @@ class TimelinesController < ApplicationController
   end
   end
 
+  def update
+      # @timeline.events << params[:event_name]
+      # @timeline.events.artists << params[:artist_name]
+  respond_to do |format|
+    if @timeline.update(timeline_params)
+      format.html { redirect_to @timeline, notice: 'Timeline was successfully updated.' }
+      format.json { head :no_content }
+    else
+      format.html { render action: 'edit' }
+      format.json { render json: @timeline.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
   private
 
   def set_timeline
@@ -32,6 +55,6 @@ class TimelinesController < ApplicationController
   end
 
   def timeline_params
-    params.require(:timeline).permit(:title, :birthyear)
+    params.require(:timeline).permit(:title, :birthyear, events_attributes: [:id, :event_name, :year], artists_attributes: [:id, :name])
   end
 end
--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,5 +1,6 @@
 class Timeline < ActiveRecord::Base
   has_many :events
   has_many :artists, through: :events
+  # accepts_nested_attributes_for :events, reject_if: :all_blank, allow_destroy: true, :update_only => true
 
 end
