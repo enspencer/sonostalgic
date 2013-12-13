7bd32308f17145058c4eecda1a9a8bb23a0ea00b
Emma Spencer <emma.n.spencer@gmail.com>
2013-12-13 17:00:11 -0500

    testing different ways of building form

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,16 +2,13 @@ class TimelinesController < ApplicationController
   before_action :set_timeline, only: [:show, :update]
 
   def index
+    @timelines = Timeline.all
   end
 
   def show
     @timeline = Timeline.find(params[:id])
     @events = @timeline.events
-    @artists = []
-    @timeline.events.each do |event|
-      @artists << event.artists
-    end
-    
+
   end
 
   def new
@@ -19,10 +16,20 @@ class TimelinesController < ApplicationController
   end
 
   def create
-    @timeline = Timeline.new(timeline_params)
+    binding.pry
+    @timeline = Timeline.new(title: timeline_params[:title], birthyear: timeline_params[:birthyear])
+    @timeline.events.build(timeline_params[:events])
+    @timeline.artists.build(timeline_params[:artists])
+    
+    # @artists = []
+    # @timeline.events.each do |event|
+    #   @artists << event.artists
+    # end
+
     if(!params.has_key?(:title))
       @timeline.title = @timeline.id
     end
+
     respond_to do |format|
     if @timeline.save
       format.html { redirect_to @timeline, notice: 'timeline was successfully created.' }
@@ -55,6 +62,12 @@ end
   end
 
   def timeline_params
-    params.require(:timeline).permit(:title, :birthyear, events_attributes: [:id, :event_name, :year], artists_attributes: [:id, :name])
+    params.require(:timeline).permit(:title, :birthyear, events: [:event_name, :year], artists: [:artist_name])
   end
+
+  # def event_params
+  #   params.require(timeline[:events]).permit(:event_name, :year)
+  # end
+
+ # , events_attributes: [:id, :event_name, :year], artists_attributes: [:id, :name]
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :timeline
   has_and_belongs_to_many :artists
+
+  accepts_nested_attributes_for :artists
 end
